cea5801e9d33e70148b77b66507ff1bf7b33de95
vytautassa-pixel <vytautassa@wix.com>
2026-03-16 17:10:22 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] || '00112233';
-
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: mode === 'production' ? `/${repoName}/` : '/',
-}));
+  base: '/00112233/',
+})
