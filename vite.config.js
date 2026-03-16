96a524d2a201c9a008d62a513dea32d21381e0e1
vytautassa-pixel <vytautassa@wix.com>
2026-03-16 16:53:55 +0200

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-var repoName = 'svg-extrusion-studio';
-export default defineConfig({
-    plugins: [react()],
-    base: process.env.NODE_ENV === 'production' ? "/".concat(repoName, "/") : '/',
-});
